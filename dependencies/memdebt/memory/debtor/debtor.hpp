815771b3cac9f4f68c61134269575355408c8380
APGR22 <AZHAR_Player@outlook.com>
2026-06-15 18:47:41 +0700

    Add memdebt dependency.

---
+++ dependencies/memdebt/memory/debtor/debtor.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <memory>
+#include "../creditor/type.hpp"
+
+namespace memdebt::memory::debtor
+{
+    template<typename T>
+    class Debtor 
+    {
+        private:
+            std::weak_ptr<T> __borrow;
+            T *__item;
+            creditor::type<T>::const_iterator __it;
+
+        public:
+            Debtor(
+                const std::weak_ptr<T> &borrow,
+                T *item,
+                const creditor::type<T>::const_iterator &it
+            )
+            : __borrow(borrow), __item(item), __it(it)
+            {}
+
+            T &get() const
+            {
+                return *(this->__item);
+            }
+
+            const auto &get_it_pos() const
+            {
+                return this->__it;
+            }
+
+            bool check() const
+            {
+                return !(this->__borrow.expired());
+            }
+
+            T *operator->() const
+            {
+                return this->__item;
+            }
+    };
+}
